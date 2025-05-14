a97a4b9a861999963504e0dda2cffaed66761a51
Anagnostakis Ioannis <elahistos@yahoo.gr>
2025-05-15 01:19:52 +0300

    remove subtitlecomposer. get the fuck out of here
    Signed-off-by: Anagnostakis Ioannis <elahistos@yahoo.gr>

--- AthOS/REPO-AthOS/subtitlecomposer/work/subtitlecomposer-0.8.0/pkg/mingw/github-upload-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Build
-build=pkg/mingw/docker-build.sh
-echo -e "Starting docker build '\e[1;39m$build\e[m'..."
-$build
-
-pkg/misc/github-release.sh win32 "$PWD/build/SubtitleComposerSetup.exe"
--- AthOS/REPO-AthOS/subtitlecomposer/work/subtitlecomposer-0.8.0/src/speechplugins/pocketsphinx/plugin-config.h.cmake
+++
@@ -1,6 +0,0 @@
-#ifndef CONFIG_H
-#define CONFIG_H
-
-#define POCKETSPHINX_MODELDIR "@POCKETSPHINX_MODELDIR@"
-
-#endif
